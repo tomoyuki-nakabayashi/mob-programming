a2e665b40f14efd65b893c280399120022c06d38
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2017-10-07 17:00:08 +0900

    artifacts

--- round-2/diff.py
+++ round-2/diff.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 class ArgumentsErrors(ValueError):
     pass
@@ -11,9 +12,21 @@ def get_input_files(arguments):
 
 
 def read_file(filename):
-    pass
-        
+    with open (filename, "r") as fd :
+        line = fd.readline()
+#        for line in fd:
+        return line;
 
+def parse_text(text):
+    text = text.replace("\n", "")
+    text = text.replace(".", "")
+    words = text.split(" ")
+
+    return words
+
+def diff(cont1, cont2):
+    if(cont1 == cont2):
+        return "I'm batman www!", "I'm batman www!"
 
 if __name__ == '__main__':
     file1, file2 = get_input_files(sys.argv)
--- round-2/diff_test.py
+++ round-2/diff_test.py
@@ -37,11 +37,36 @@ class DiffTest(unittest.TestCase):
     def test_get_input_files_errorCase1(self):
         self.assertRaises(diff.ArgumentsErrors, get_input_files, ["diff.py", "hoge.txt"])
         
-    # get_input_files error case
     def test_read_file_success(self):
-        expected = "file contents"
+        expected = "file contents\n"
         actual = diff.read_file("hoge.txt")
-        self.assertEqual(expected,actual)
+        self.assertEqual(expected, actual)
+        
+        expected = "file contents another\n"
+        actual = diff.read_file("foo.txt")
+        self.assertEqual(expected, actual)
+
+    def test_read_file_error(self):
+        self.assertRaises(FileNotFoundError, diff.read_file, "invalid_file.txt")
+
+    def test_parse_text_success(self):
+        expected = ["aaaabcd", "taiwannihon", "tonkotsu"]
+        actual = diff.parse_text("aaaabcd taiwannihon tonkotsu.\n")
+        self.assertEqual(expected, actual)
+        expected = ["I'm", "batman", "www!"]
+        actual = diff.parse_text("I'm batman www!")
+        self.assertEqual(expected, actual)
+
+    def test_diff_case_equal(self):
+        expected = "I'm batman www!"
+        actual1, actual2 = diff.diff(["I'm", "batman", "www!"],["I'm", "batman", "www!"])
+        self.assertEqual(expected, actual1)
+        self.assertEqual(expected, actual2)
+
+
+
+
+
 
 
 if __name__ == '__main__':
